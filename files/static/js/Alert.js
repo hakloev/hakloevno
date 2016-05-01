b477819db91c0c442b3d69ea68df8980c4db1b27
Håkon Ødegård Løvdal <me@hakloev.no>
2016-05-01 21:14:59 +0200

    Starts weather app and alert dismiss

---
+++ files/static/js/Alert.js
@@ -0,0 +1,52 @@
+var Alert = (function () {
+    
+    var closeTag = '[data-close="alert"]';
+    
+    var closeAlert = function (e) {
+        e.preventDefault();
+        console.log('alert click');
+        console.log('this', this);
+        console.log('e', e.target);
+        var parent = e.target;
+        // Lacks matchesSelector
+        while ((parent = parent.parentElement) && !((parent.matches('.hn-alert')))) {
+            console.log('next');
+        }
+        console.log('found', parent);
+        parent.parentElement.removeChild(parent);
+    };
+
+
+    return {
+        // This function is called on a window load event
+        listener: function () {
+            var alerts = document.getElementsByClassName('hn-alert-dismiss');
+            if (alerts.length > 0) {
+                console.log('Add alert listeners');
+                for (var i = 0; i < alerts.length; i++) {
+                    alerts[i].addEventListener('click', closeAlert)
+                }
+            }
+        }
+    }
+
+    
+})();
+
+window.addEventListener('load', function () {
+   Alert.listener();
+});
+
+// TODO: Add support for new DOM nodes as well
+// var onNewElement = function () {
+//   try {
+//       switch (e.target.id) {
+//           case 'hn-alert': Alert.listener; break;
+//           default:
+//               console.log('def');
+//               break;
+//       }
+//   }  catch (exeption) {}
+// };
+
+//document.addEventListener('DOMNodeInserted', onNewElement, false);
--- files/static/js/Weather.js
+++ files/static/js/Weather.js
@@ -3,7 +3,12 @@ var Weather = (function () {
     var apiURL = '/static/files/varsel.xml';
 
     var success = function (data) {
-        document.getElementById('weather').innerHTML = JSON.stringify(data);
+        document.getElementById('forecast').innerHTML = data.text;
+
+        var detailsString = "";
+        detailsString += (data.temperature + "°C <br>");
+        detailsString += data.windSpeedText + " ";
+        document.getElementById('forecast-details').innerHTML = detailsString;
     };
 
     var getWeatherData = function () {
@@ -18,6 +23,7 @@ var Weather = (function () {
             var forecastNode = doc.getElementsByTagName('forecast')[0];
             var newestForecastNode = forecastNode.getElementsByTagName('tabular')[0].getElementsByTagName('time')[0];
 
+            var text = forecastNode.getElementsByTagName('text')[0].getElementsByTagName('body')[0].childNodes[0].data;
             var symbol =  newestForecastNode.getElementsByTagName('symbol')[0].getAttribute('number');
             var windDirection = newestForecastNode.getElementsByTagName('windDirection')[0].getAttribute('name');
             var windSpeed =  newestForecastNode.getElementsByTagName('windSpeed')[0].getAttribute('mps');
@@ -26,6 +32,7 @@ var Weather = (function () {
             var pressure = newestForecastNode.getElementsByTagName('pressure')[0].getAttribute('value');
 
             success({
+                'text': text,
                 'symbol': symbol,
                 'windDirection': windDirection,
                 'windSpeed': windSpeed,
